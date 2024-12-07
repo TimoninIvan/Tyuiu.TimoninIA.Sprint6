b38edf45a1ae521ffa2f7c8ed975133fdcda6fd2
Ivan Timonin <Tyuiu.TimoninIA@outlook.com>
2024-12-07 18:53:12 +0500

    Task3

--- Tyuiu.TimoninIA.Sprint6.Task3.V25.Lib/DataService.cs
+++ Tyuiu.TimoninIA.Sprint6.Task3.V25.Lib/DataService.cs
@@ -5,48 +5,22 @@ namespace Tyuiu.TimoninIA.Sprint6.Task3.V25.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-
             int rows = matrix.GetUpperBound(0) + 1;
-            int colums = matrix.Length / rows;
-
-
-
+            int cols = matrix.Length / rows;
+            int[,] res = new int[rows, cols];
             for (int i = 0; i < rows; i++)
             {
-
-                for (int j = 0; j < colums; j++)
+                for (int j = 0; j < cols; j++)
                 {
-                    if (matrix[1, i] % 2 == 0)
+                    if ((matrix[i, j] % 2 == 0) && (i == 2))
                     {
-                        matrix[1, i] = 0;
+                        res[i, j] = 0;
                     }
-
+                    else
+                        res[i, j] = matrix[i, j];
                 }
-
-
-
-
             }
-            return matrix;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            return res;
         }
     }
 }
